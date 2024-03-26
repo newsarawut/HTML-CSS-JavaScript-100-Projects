61f3371f1b271970e57a448c2fb04a16ac713443
newsarawut <sarawutch572@gmail.com>
2024-03-26 23:11:09 +0700

    Rotating Image Gallery

---
+++ 62.Rotating Image Gallery/app.js
@@ -0,0 +1,22 @@
+// imageContainer
+const imageContainer = document.querySelector(".image-container");
+// prevBtn
+const prevBtn = document.getElementById("prev");
+// nextBtn
+const nextBtn = document.getElementById("next");
+
+let x = 0;
+
+prevBtn.addEventListener("click", () => {
+  x = x + 45;
+  rotate();
+});
+
+nextBtn.addEventListener("click", () => {
+  x = x - 45;
+  rotate();
+});
+
+function rotate() {
+  imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
